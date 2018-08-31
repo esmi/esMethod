bacda5f7ea9d78a2fcfcb57897a62c368f3569d9
evan chen <evansmile@hotmail.com>
2018-08-31 16:05:35 +0800

    remove comment code.

--- example/ex1.php
+++ example/ex1.php
@@ -1,13 +1,9 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-// include_once "../src/abstractMethod.php";
-// include_once "../src/iMethod.php";
-// include_once "../src/method.php";
-// include_once "../src/methodUtils.php";
+
 use Esmi\EsMethod;
 
 class ex1 implements \Esmi\EsMethod\IMethod {
-//    class ex1 implements \Esmi\esMethod\IMethod {
 
     use \Esmi\esMethod\methodUtils;
     //use methodUtils;
